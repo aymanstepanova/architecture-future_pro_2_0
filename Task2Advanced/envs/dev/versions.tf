3fd9b857190d5d076435183687d8684b2b98c688
aymanstepanova <ayman.stepanova@gmail.com>
2026-04-23 01:09:05 +0300

    Task2Advanced: описание workflow для создания ВМ и сохранения статуса в S3
    - создание ВМ в github actions: init, plan, apply, destroy
    - сохранение статуса в S3 
    - переменные в Github Variables

---
+++ Task2Advanced/envs/dev/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  backend "s3" {}
+
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.100.0"
+    }
+  }
+}
